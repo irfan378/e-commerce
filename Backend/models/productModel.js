09a0737e0bc09483e2931f0e329658142f209cb4
Irfan Farooq <irfandominate@gmail.com>
2022-02-04 21:10:40 +0530

    created product api

---
+++ Backend/models/productModel.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter the product name"],
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, "Please enter the product description"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Please enter the product price"],
+        maxlength: [10, "Price should be less than 10 digits"]
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    images: [{
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    }],
+    category: {
+        type: String,
+        required: [true, "Please Enter Product Category"]
+    },
+    Stock: {
+        type: Number,
+        required: [true, "Please Enter Product Stock"],
+        maxlength: [10, "Stock should be less than 10 characters"],
+        default: 1
+    },
+    noOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+module.exports = mongoose.model("Product", productSchema);
--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/product");
+const { getAllProducts, createProduct } = require("../controllers/product");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
+router.route("/products/new").post(createProduct);
 
 
 module.exports = router;
