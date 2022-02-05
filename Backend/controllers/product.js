9dc9bbadaaf3c4cd3a98ead4807ff5252af2addc
Irfan Farooq <irfandominate@gmail.com>
2022-02-05 10:52:07 +0530

    added get product and update product api

--- Backend/controllers/product.js
+++ Backend/controllers/product.js
@@ -1,6 +1,6 @@
 const Product = require('../models/productModel');
 
-// Create Product
+// Create Product-- Admin
 exports.createProduct = async (req, res, next) => {
     const product = await Product.create(req.body);
     res.status(201).json({
@@ -8,6 +8,31 @@ exports.createProduct = async (req, res, next) => {
         product
     });
 }
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({ message: "Route is working fine" })
+// get all products
+exports.getAllProducts = async (req, res) => {
+    const products = await Product.find();
+    res.status(200).json({
+        success: true,
+        products
+    })
+}
+
+// Update a product -- admin
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+    res.status(200).json({
+        success: true,
+        product
+    })
 }
