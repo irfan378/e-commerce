9e01d000b869990a584b9b44c96d6cce0914bcc6
Irfan Farooq <irfandominate@gmail.com>
2022-02-06 20:38:57 +0530

    added delete and product details api

--- Backend/controllers/product.js
+++ Backend/controllers/product.js
@@ -36,3 +36,35 @@ exports.updateProduct = async (req, res, next) => {
         product
     })
 }
+
+// Delete product
+exports.deleteProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+    await product.remove();
+    res.status(200).json({
+        success: true,
+        message: "Product  Deleted Sucessfully"
+    })
+}
+
+// get product details
+exports.getProductDetails = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+} 
