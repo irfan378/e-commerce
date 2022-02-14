5e37bbaa5838f7c4189318c44039f9d4c8df0336
Irfan Farooq <irfandominate@gmail.com>
2022-02-14 21:36:10 +0530

    login

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -14,8 +14,33 @@ exports.registerUser = catchAsyncError(async (req, res, next) => {
             url: "profilepicUrl",
         },
     });
+    const token = user.getJWTToken();
     res.status(201).json({
         success: true,
-        user
+        token
+    });
+})
+
+// Login user
+exports.login = catchAsyncError(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    //  checking if user has given password and email both
+    if (!email || !password) {
+        return next(new ErrorHandler("Please Enter email and password", 400));
+    }
+    const user = await User.findOne({ email }.select("+password"));
+
+    if (!user) {
+        return next(new ErrorHandler("Invalid email or password", 401))
+    }
+    const isPasswordMatched = user.comparePassword(password);
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler("Invalid email or password", 401))
+    }
+    const token = user.getJWTToken();
+    res.status(200).json({
+        success: true,
+        token
     });
 })
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { registerUser } = require("../controllers/userController");
+const { registerUser, login } = require("../controllers/userController");
 const router = express.Router();
 
 router.route("/register").post(registerUser);
+router.route("/login").post(login);
 
 module.exports = router;
