1efa1baa808e0042df538c5937f0be6b5235a0b9
Irfan Farooq <irfandominate@gmail.com>
2022-02-15 21:22:53 +0530

    fixed some bugs in registration and login apis

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -8,13 +8,14 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, "Plaese enter your name"],
         maxlength: [50, "Name must be less than 50 characters"],
-        minlength: [2, "Name must be at least 2 characters"]
+        minlength: [2, "Name must be at least 2 characters"],
     },
     email: {
         type: String,
         required: [true, "Please Enter your email"],
         validate: [validator.isEmail, 'Please Enter a valid Email'],
-        unique: true
+        unique: true,
+
     },
     password: {
         type: String,
