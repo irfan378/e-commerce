79782eba3b76a21ec0ddc271f9b90cbecae960b2
Irfan Farooq <irfandominate@gmail.com>
2022-02-19 20:40:59 +0530

    forgot password

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require('crypto');
 
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -62,5 +63,14 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
 }
 
 // Generating Reset Password Token
+userSchema.methods.getResetPasswordToken = function () {
+    // Generating token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+    this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+}
 
 module.exports = mongoose.model("User", userSchema);
