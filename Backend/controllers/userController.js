cd372abddfd861a3bb9df0d0281009d807d80e2c
Irfan Farooq <irfandominate@gmail.com>
2022-02-22 17:37:53 +0530

    get user details and update password

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -117,3 +117,35 @@ exports.resetPassword = catchAsyncError(async (req, res, next) => {
     sendToken(user, 200, res);
 
 })
+
+
+// Get User Details
+exports.getUserDetails = catchAsyncError(async (req, res, next) => {
+
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
+
+// Update User Password
+exports.updatePassword = catchAsyncError(async (req, res, next) => {
+
+    const user = await User.findById(req.user.id).select("+password");
+
+    const isPasswordMatched = user.comparePassword(req.body.oldPassword);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler("Old Password is incorrect", 400))
+    }
+    if (req.body.newPassword != req.body.confirmPassword) {
+        return next(new ErrorHandler("New Password and Confirm Password do not match", 400))
+    }
+    user.password = req.body.newPassword;
+
+    await user.save();
+
+    sendToken(user, 200, res);
+})
