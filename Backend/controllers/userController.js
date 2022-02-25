b4e02c1a99df3faedd5890ecdd6ac1daddd77a8b
Irfan Farooq <irfandominate@gmail.com>
2022-02-25 11:39:49 +0530

    update user role and delte user for admin

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -192,3 +192,41 @@ exports.getUser = catchAsyncError(async (req, res, next) => {
         user
     });
 })
+
+// Update User role --Admin
+exports.updateUserRole = catchAsyncError(async (req, res, next) => {
+
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role
+    }
+    const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+    if (!user) {
+        return next(new ErrorHandler("User doesn't exist", 404));
+    }
+
+    res.status(200).json({
+        success: true
+    })
+})
+
+// Delete User --Admin
+exports.deleteUser = catchAsyncError(async (req, res, next) => {
+
+    const user = await User.findById(req.params.id);
+    if (!user) {
+        return next(new ErrorHandler("User doesn't exist", 404));
+    }
+    await user.remove();
+
+    res.status(200).json({
+        success: true,
+        message: "User deleted successfully"
+    })
+})
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { route } = require("express/lib/application");
-const { registerUser, login, logout, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile, getAllUser, getUser } = require("../controllers/userController");
+const { registerUser, login, logout, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile, getAllUser, getUser, updateUserProfile, updateUserRole, deleteUser } = require("../controllers/userController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
@@ -22,6 +22,7 @@ router.route("/me/update").put(isAuthenticatedUser, updateProfile)
 
 router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("admin"), getAllUser);
 
-router.route("/admin/user/:id").get(isAuthenticatedUser, authorizeRoles("admin"), getUser);
+router.route("/admin/user/:id").get(isAuthenticatedUser, authorizeRoles("admin"), getUser).put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole).delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
+
 
 module.exports = router;
