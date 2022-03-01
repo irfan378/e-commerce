798e292af5344ae44907812f2d8c9bad28fbe0ca
Irfan Farooq <irfandominate@gmail.com>
2022-03-01 17:10:21 +0530

    added create order route

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -3,4 +3,16 @@ const Product = require('../models/productModel');
 const ErrorHandler = require('../Utils/ErrorHandler');
 const catchAsyncError = require('../middleware/catchAsyncError');
 
-// Create new Order
+// Create new Orde
+exports.createOrder = catchAsyncError(async (req, res, next) => {
+    const { shippingInfo, orderItems, paymentInfo, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body;
+
+    const order = await Order.create({
+        shippingInfo, orderItems, paymentInfo, itemsPrice, taxPrice, shippingPrice, totalPrice, paidAt: Date.now(), user: req.user._id
+    })
+
+    res.status(201).json({
+        success: true,
+        order
+    })
+})
