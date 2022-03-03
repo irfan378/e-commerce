5427e6e111214112c9e580cc0bab59b7cea397ce
Irfan Farooq <irfandominate@gmail.com>
2022-03-03 20:39:05 +0530

    added orders api

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -3,7 +3,7 @@ const Product = require('../models/productModel');
 const ErrorHandler = require('../Utils/ErrorHandler');
 const catchAsyncError = require('../middleware/catchAsyncError');
 
-// Create new Orde
+// Create new Order
 exports.createOrder = catchAsyncError(async (req, res, next) => {
     const { shippingInfo, orderItems, paymentInfo, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body;
 
@@ -17,6 +17,7 @@ exports.createOrder = catchAsyncError(async (req, res, next) => {
     })
 })
 
+// Get logged in user orders
 exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
 
     const order = await Order.findById(req.params.id).populate("user", "name email");
@@ -24,6 +25,17 @@ exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
     if (!order) {
         return next(new ErrorHandler(404, "Order not found"));
     }
+    res.status(200).json({
+        success: true,
+        order
+    })
+})
+
+// My orders
+
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+
+    const order = await Order.find({ user: req.user._id })
     res.status(200).json({
         success: true,
         order
--- Backend/routes/order.js
+++ Backend/routes/order.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { createOrder } = require("../controllers/orderController");
+const { createOrder, myOrders } = require("../controllers/orderController");
 const router = express.Router();
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 router.route("/order/new").post(isAuthenticatedUser, createOrder)
+
+router.route("/order/:id").get(isAuthenticatedUser, authorizeRoles("admin"));
+
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
 module.exports = router;
