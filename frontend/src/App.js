1b69ebf865ddc32b9365b8801dfb5858f80a64ea
Irfan Farooq <irfandominate@gmail.com>
2022-03-12 10:23:53 +0530

    Styled navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 
 import React from 'react'
 import Header from "./components/layout/Header.js";
+import { BrowserRouter as Router } from 'react-router-dom';
+import WebFont from 'webfontloader';
+
+
 
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Roboto:300,400,500,700', 'Material Icons']
+      }
+    });
+  }, [])
   return (
-    <Header />
+    <Router>
+      <Header />
+    </Router>
   );
 }
 
