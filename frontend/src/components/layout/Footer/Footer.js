1efcb07b68f866f03a7125df70f2b115293a946c
Irfan Farooq <irfandominate@gmail.com>
2022-03-14 20:27:43 +0530

    removed some packages

--- frontend/src/components/layout/Footer/Footer.js
+++ frontend/src/components/layout/Footer/Footer.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import "./Footer.css"
-import styles from "../../../App.css"
 
 const Footer = () => {
     return (
         <footer className='my-10 p-2 bg-black text-white'>
-            <div className="leftFooter">
+            <div className="bg-black">
                 <h4>Ecommerce</h4>
                 <p>High quality and fast delivery is our priority</p>
                 <p>Copyrights 2022 &copy; Irfan Farooq</p>
--- frontend/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./src/components/layout/Footer/.{js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
