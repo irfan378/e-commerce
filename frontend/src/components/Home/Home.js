30883209ec623f04efe89217528ccdf635be0c2c
Irfan Farooq <irfandominate@gmail.com>
2022-03-17 19:44:18 +0530

    product component

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,13 @@
 import React, { Fragment } from 'react'
 import { CgMouse } from 'react-icons/cg'
 import "./Home.css"
+import Product from "./Product.js"
+const product = {
+    name: "Blue t shirt",
+    images: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
+    price: "$5",
+    _id: "340993934ri"
+};
 const Home = () => {
     return <Fragment>
         <div className="banner">
@@ -14,6 +21,9 @@ const Home = () => {
             </a>
         </div>
         <h2 className="homeHeading">Featured Products</h2>
+        <div className="container" id="container">
+            <Product product={product} />
+        </div>
     </Fragment>
 }
 
