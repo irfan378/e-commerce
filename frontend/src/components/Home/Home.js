45647065e19e3366ef4d69744fce6ee7d9e06123
Irfan Farooq <irfandominate@gmail.com>
2022-03-18 16:41:24 +0530

    added redux

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react'
 import { CgMouse } from 'react-icons/cg'
 import "./Home.css"
 import Product from "./Product.js"
+import MetaData from '../layout/MetaData'
+
 const product = {
     name: "Blue t shirt",
     images: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
@@ -10,6 +12,7 @@ const product = {
 };
 const Home = () => {
     return <Fragment>
+        <MetaData title="e-commerce" />
         <div className="banner">
             <p>Welcome to Ecommerce</p>
             <h1>You can find products below</h1>
@@ -22,6 +25,14 @@ const Home = () => {
         </div>
         <h2 className="homeHeading">Featured Products</h2>
         <div className="container" id="container">
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
             <Product product={product} />
         </div>
     </Fragment>
