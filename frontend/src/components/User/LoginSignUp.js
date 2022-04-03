3094aadc43fd2a9de07b41be6f4c90879597426d
Irfan Farooq <irfandominate@gmail.com>
2022-04-03 12:22:22 +0530

    signupform

--- frontend/src/components/User/LoginSignUp.js
+++ frontend/src/components/User/LoginSignUp.js
@@ -4,6 +4,7 @@ import Loader from "../layout/Loader/Loader"
 import { Link } from "react-router-dom"
 import MailOutlineIcon from "@material-ui/icons/MailOutline"
 import LockOpenIcon from "@material-ui/icons/LockOpen"
+import FaceIcon from "@material-ui/icons/Face"
 const LoginSignUp = () => {
     const loginTab = useRef(null);
     const registerTab = useRef(null);
@@ -11,13 +12,47 @@ const LoginSignUp = () => {
 
     const [loginEmail, setLoginEmail] = useState("");
     const [loginPassword, setLoginPassword] = useState("");
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+    const { name, email, password } = user;
+    const [avatar, setAvatar] = useState();
+    const [avatarPreview, setAvatarPreview] = useState("/profile.jpg")
 
 
     const loginSubmit = () => {
         console.log("Form Submitted");
     }
 
+    const registerSubmit = (e) => {
+        e.preventDefault();
+        const myForm = new FormData();
 
+        myForm.set("name", name)
+        myForm.set("email", email)
+        myForm.set("password", password)
+        myForm.set("avatar", avatar)
+        console.log("Sign up form submitted")
+    }
+
+    const registerDataChange = (e) => {
+        if (e.target.name === "avatar") {
+            const reader = new FileReader();
+
+            reader.onload = () => {
+                if (reader.readyState == 2) {
+                    setAvatarPreview(reader.result);
+                    setAvatar(reader.result);
+                }
+            };
+            reader.readAsDataURL(e.target.files[0]);
+        }
+        else {
+            setUser({ ...user, [e.target.name]: e.target.value });
+        }
+    }
     const switchTabs = (e, tab) => {
         if (tab === "login") {
             switcherTab.current.classList.add("shiftToNeutral")
@@ -56,6 +91,25 @@ const LoginSignUp = () => {
                     </div>
                     <Link to="/password/forget">Forget Password ?</Link>
                     <input type="submit" value="Login" className="loginBtn" />
+                </form>
+                <form className='signUpForm' ref={registerTab} encType="multipart/form-data" onSubmit={registerSubmit}>
+                    <div className="signUpName">
+                        <FaceIcon />
+                        <input type="text" placeholder='Name' required name="name" value={name} onChange={registerDataChange} />
+                    </div>
+                    <div className="singUpEmail">
+                        <MailOutlineIcon />
+                        <input type="email" placeholder='Email' required name='email' value={email} onChange={registerDataChange} />
+                    </div>
+                    <div className="signUpPassword">
+                        <LockOpenIcon />
+                        <input type="password" placeholder='Password' required name='password' value={password} onChange={registerDataChange} />
+                    </div>
+                    <div id="registerImage">
+                        <img src={avatarPreview} alt="Avatar Preview" />
+                        <input type="file" name="avatar" accept="image/*" onChange={registerDataChange} />
+                    </div>
+                    <input type="submit" value="Register" className="signUpBtn" />
                 </form>
             </div>
         </div>
