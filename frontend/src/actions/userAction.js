bd57fabb187e44ccc3fd53fdeefcdd16fa594032
Irfan Farooq <irfandominate@gmail.com>
2022-04-06 18:19:32 +0530

    login form

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,4 +1,4 @@
-import { LOGIN_REQUEST, LOGIN_FAIL, LOGIN_SUCCESS, CLEAR_ERRORS } from "../constants/userConstant";
+import { LOGIN_REQUEST, LOGIN_FAIL, LOGIN_SUCCESS, CLEAR_ERRORS, REGISTER_USER_REQUEST, REGISTER_USER_FAIL, REGISTER_USER_SUCCESS } from "../constants/userConstant";
 import axios from "axios"
 
 export const login = (email, password) => async (dispatch) => {
@@ -9,6 +9,26 @@ export const login = (email, password) => async (dispatch) => {
         dispatch({ type: LOGIN_SUCCESS, payload: data.user })
     } catch (error) {
         dispatch({ type: LOGIN_FAIL, payload: error.response.data.message })
+    }
+}
+export const register = (userData) => async (dispatch) => {
+    try {
+        dispatch({ type: REGISTER_USER_REQUEST });
+        const config = {
+            headers: {
+                "Content-Type": "multipart/form - data"
+            }
+        };
+        const { data } = await axios.post('/api/v1/register', userData, config);
+        dispatch({
+            type: REGISTER_USER_SUCCESS,
+            payload: data.user
+        })
+    } catch (error) {
+        dispatch({
+            type: REGISTER_USER_FAIL,
+            payload: error.response.data.message
+        })
     }
 }
 export const clearErrors = () => async (dispatch) => {
