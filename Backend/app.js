955e98d43e9824c1354384ab67b12e2bd89f9d56
Irfan Farooq <irfandominate@gmail.com>
2022-04-29 15:13:42 +0530

    resolved

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,9 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload")
 const errorMiddleWare = require('./middleware/error')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: "backend/config/config.env" })
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
@@ -14,9 +17,11 @@ app.use(fileUpload());
 const product = require("./routes/product");
 const user = require('./routes/user');
 const order = require('./routes/order')
+const payment = require('./routes/paymentRoute')
 app.use("/api/v1", product);
 app.use("/api/v1", user)
 app.use("/api/v1", order)
+app.use("/api/v1", payment)
 
 // middleware for Errors
 app.use(errorMiddleWare)
