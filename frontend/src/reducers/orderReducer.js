6bdc051759b9f5f04fce5a497b1f8ae4539ee765
Irfan Farooq <irfandominate@gmail.com>
2022-05-12 18:45:20 +0530

    dashboard

--- frontend/src/reducers/orderReducer.js
+++ frontend/src/reducers/orderReducer.js
@@ -13,7 +13,7 @@ export const newOrderReducer = (state = {}, action) => {
             }
         case CREATE_ORDER_FAIL:
             return {
-                loading: true,
+                loading: false,
                 error: action.payload
             }
         case CLEAR_ERRORS:
@@ -38,7 +38,7 @@ export const myOrdersReducer = (state = { orders: [] }, action) => {
             }
         case MY_ORDERS_FAIL:
             return {
-                loading: true,
+                loading: false,
                 error: action.payload
             }
         case CLEAR_ERRORS:
@@ -64,7 +64,7 @@ export const orderDetailReducer = (state = { order: {} }, action) => {
             }
         case ORDER_DETAILS_FAIL:
             return {
-                loading: true,
+                loading: false,
                 error: action.payload
             }
         case CLEAR_ERRORS:
