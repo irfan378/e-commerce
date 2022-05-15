f95df32a774939bc907bba74adf88c2b48827265
Irfan Farooq <irfandominate@gmail.com>
2022-05-15 13:53:48 +0530

    solved some errors

--- frontend/src/components/Route/ProtectedRoute.js
+++ frontend/src/components/Route/ProtectedRoute.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
 
 const ProtectedRoute = () => {
-    const { isAuthenticated } = useSelector((state) => state.user)
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 
+    const { isAuthenticated } = useSelector((state) => state.user)
+    const location = useLocation()
+    if (isAuthenticated) {
+        return <Outlet></Outlet>
+    }
+
+    return <Navigate to="/login" state={{ from: location }} />
 }
 
 export default ProtectedRoute
