391e1a2123281a8b1f2cde011cdabb7e0295537f
Irfan Farooq <irfandominate@gmail.com>
2022-05-25 19:20:16 +0530

    update product admin

--- Backend/controllers/product.js
+++ Backend/controllers/product.js
@@ -71,6 +71,35 @@ exports.updateProduct = catchAsyncError(async (req, res, next) => {
             message: "Product not found"
         })
     }
+    // images start here
+    let images = [];
+    if (typeof req.body.images === 'string') {
+        images.push(req.body.images);
+    } else {
+        images = req.body.images;
+    }
+    if (images !== undefined) {
+        // Deleting images from cloudinary
+        for (let i = 0; i < product.images.length; i++) {
+            await cloudinary.v2.uploader.destroy(product.images[i].public_id)
+
+        }
+    }
+
+    const imagesLink = [];
+
+    for (let i = 0; i < images.length; i++) {
+        const result = await cloudinary.v2.uploader.upload(images[i], {
+            folder: "products"
+        })
+
+        imagesLink.push({
+            public_id: result.public_id,
+            url: result.secure_url
+        })
+
+        req.body.images = imagesLink;
+    }
     product = await Product.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true,
