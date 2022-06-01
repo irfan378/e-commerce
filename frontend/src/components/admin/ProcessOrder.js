609551dcab9f9c3ad6aa2f5d6a4c8ec307d6f633
irfan <irfandominate@gmail.com>
2022-06-01 18:58:05 +0530

    Edit order

--- frontend/src/components/admin/ProcessOrder.js
+++ frontend/src/components/admin/ProcessOrder.js
@@ -1,51 +1,78 @@
-import React, { Fragment } from 'react'
-import { useSelector } from 'react-redux'
+import React, { Fragment, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import MetaData from '../layout/MetaData'
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate, useParams } from "react-router-dom"
 import { Typography } from '@material-ui/core'
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney"
+import Sidebar from "./Sidebar"
+import { clearErrors, getOrderDetails } from '../../actions/orderAction'
+import { useAlert } from 'react-alert'
 
 const ProcessOrder = () => {
-    const { shippingInfo, cartItems } = useSelector((state) => state.cart)
-    const { user } = useSelector((state) => state.user)
+    const { order,error,loading } = useSelector((state) => state.orderDetails)
+   
     const navigate = useNavigate();
-    const subtotal = cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0);
-    const shippingCharges = subtotal > 1000 ? 0 : 200;
-    const tax = subtotal * 0.18;
-    const totalPrice = subtotal * tax + shippingCharges;
-    const address = `${shippingInfo.address},${shippingInfo.city},${shippingInfo.state},${shippingInfo.pinCode},${shippingInfo.country}`
+    const dispatch=useDispatch();
+    const params=useParams();
+    const alert=useAlert();
+   
     const proceedToPayment = () => {
-        const data = {
-            subtotal, shippingCharges, tax, totalPrice
-        };
-        sessionStorage.setItem("orderInfo", JSON.stringify(data))
-        navigate("/process/payment")
     }
+    useEffect(()=>{
+        if (error) {
+            alert.error(error);
+            dispatch(clearErrors());
+        }
+        dispatch(getOrderDetails(params.id))
+    },[dispatch,alert,error,params.id])
     return <Fragment>
-        <MetaData title="Confirm Order" />
-
-        <div className="confirmOrderPage">
+        <MetaData title="Create Product" />
+        <div className="dashboard">
+            <Sidebar />
+            <div className="newProductContainer">
+            <div className="confirmOrderPage">
             <div>
                 <div className="confirmShippingArea">
                     <Typography>Shipping Info</Typography>
-                    <div className="confirmShippingAreaBox">
-                        <div>
-                            <p>Name:</p>
-                            <span>{user.name}</span>
+                    <div className="orderDetailsContainerBox">
+                            <div>
+                                <p>Name: </p>
+                                <span>{order.user && order.user.name}</span>
+                            </div>
+                            <div>
+                                <p>Phone: </p>
+                                <span>{order.shippingInfo && order.shippingInfo.PhoneNo}</span>
+                            </div>
+                            <div>
+                                <p>Address: </p>
+                                <span>{order.shippingInfo && `${order.shippingInfo.address},${order.shippingInfo.city},${order.shippingInfo.state},${order.shippingInfo.pinCode},${order.shippingInfo.country}`}</span>
+                            </div>
                         </div>
-                        <div>
-                            <p>Phone:</p>
-                            <span>{shippingInfo.PhoneNo}</span>
-                        </div>
-                        <div>
-                            <p>Address:</p>
-                            <span>{address}</span>
-                        </div>
-                    </div>
                 </div>
+                <Typography>Payment</Typography>
+                        <div className="orderDetailsContainerBox">
+                            <div>
+                                <p className={order.paymentInfo && order.paymentInfo.status === "succeeded" ? "greenColor" : "redColor"}>
+                                    {order.paymentInfo && order.paymentInfo.status === "succeeded" ? "PAID" : "NOT PAID"}
+                                </p>
+                            </div>
+                            <div>
+                                <p>Amount:</p>
+                                <span>{order.totalPrice && order.totalPrice}</span>
+                            </div>
+                        </div>
+                        <Typography>Order Status</Typography>
+                        <div className="orderDetailsContainerBox">
+                            <div>
+                                <p className={order.orderStatus && order.orderStatus === "Delivered" ? "greenColor" : "redColor"}>
+                                    {order.orderStatus && order.orderStatus}
+                                </p>
+                            </div>
+                        </div>
                 <div className="confirmCartItems">
                     <Typography>Your Cart Items:</Typography>
                     <div className="confirmCartItemsContainer">
-                        {cartItems && cartItems.map((item) => (
+                        {order.orderItems &&order.orderItems.map((item) => (
                             <div key={item.product}>
                                 <img src={item.image} alt="Product" />
                                 <Link to={`/product/${item.product}`}>
@@ -67,28 +94,34 @@ const ProcessOrder = () => {
                     <div>
                         <div>
                             <p>Subtotal:</p>
-                            <span>${subtotal}</span>
+                            <span>$34343</span>
                         </div>
                         <div>
                             <p>Shipping Charges:</p>
-                            <span>${shippingCharges}</span>
+                            <span>$3545445</span>
                         </div>
                         <div>
                             <p>GST:</p>
-                            <span>${tax}</span>
+                            <span>$5454545</span>
                         </div>
                     </div>
                     <div className="orderSummaryTotal">
                         <p>
                             <b>Total:</b>
                         </p>
-                        <span>${totalPrice}</span>
+                        <span>$2222</span>
                     </div>
                     <button onClick={proceedToPayment}>Proceed To Payment</button>
                 </div>
             </div>
         </div>
+            </div>
+        </div>
     </Fragment>
+     
+
+       
+ 
 }
 
 
